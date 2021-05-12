3811aeefee360301c639caf18e909db6886ec049
Pedro <pemoraes1@gmail.com>
2021-05-12 15:59:50 -0300

    Inclui CRUD básico
    Os pacotes: http, express, debug, body-parser foram adicionados ao projeto. Os arquivos de rota, servidor e controlador estão separados em diferentes pastas

---
+++ src/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+// Carrega as Rotas
+const indexRoutes = require('./routes/index-routes');
+const productRoutes = require('./routes/product-routes');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoutes);
+app.use('/products', productRoutes);
+
+module.exports = app;
